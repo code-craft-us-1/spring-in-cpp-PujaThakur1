9c3132ff106136508a12add0c58ccf082ba4197b
PujaThakur1 <112850341+PujaThakur1@users.noreply.github.com>
2024-08-23 02:02:21 +0000

    avg

--- stats.cpp
+++ stats.cpp
@@ -1,7 +1,23 @@
 #include "stats.h"
+#include<iostream>
+#include <numeric>
 using namespace Statistics;
+using  namespace std;
 
-Stats Statistics::ComputeStatistics(const std::vector<float>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& v ) {
     //Implement statistics here
-    ;
+    Stats ret;
+     int n = v.size(); 
+    if (n == 0) 
+        ret.average = 0;
+  
+    else { 
+        // sum of the vector elements 
+        double sum = accumulate(v.begin(), v.end(), 0.0); 
+  
+        // average of the vector elements 
+        double avg = sum / n; 
+    ret.average = avg;
+
+
 }
